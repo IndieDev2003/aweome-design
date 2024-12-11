f08dc5ebc1b8fd2a2acc5ec1e744c07debcce2e9
Gagan Suman <fbdgagansuman@gmail.com>
2024-12-11 14:15:03 +0530

    2nd

--- src/components/Hero/HeroBack.jsx
+++ src/components/Hero/HeroBack.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroBack() {
   return (
       <div className='h-screen w-full opacity-90 absolute -z-10 top-0 overflow-hidden'>
-          <img src="https://i.pinimg.com/1200x/7a/f5/3f/7af53f331da668769fce5fe4f2dbf664.jpg" className='w-full object-cover object-center' alt="" />
+          <img src="https://i.pinimg.com/1200x/7a/f5/3f/7af53f331da668769fce5fe4f2dbf664.jpg" className='h-full w-full object-cover object-center' alt="" />
     </div>
   )
 }
